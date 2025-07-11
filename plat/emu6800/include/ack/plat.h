e5f0ef02068ac33e6f948d1a857a7e0c578ecdf1
zu2 <3023910+zu2@users.noreply.github.com>
2025-07-12 03:18:03 +0900

    add file

---
+++ plat/emu6800/include/ack/plat.h
@@ -0,0 +1,17 @@
+/* $Source$
+ * $State$
+ * $Revision$
+ */
+
+#ifndef _ACK_PLAT_H
+#define _ACK_PLAT_H
+
+#define ACKCONF_WANT_STDIO_FLOAT 0
+#define ACKCONF_WANT_EMULATED_TIME 0
+#define ACKCONF_WANT_EMULATED_POPEN 0
+
+/* We have a very small address space, so override the default buffer size. */
+
+#define BUFSIZ 256
+
+#endif
