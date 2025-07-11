af2ae446d5d5e051b96cdeae40ec093c24252498
zu2 <3023910+zu2@users.noreply.github.com>
2025-07-12 03:03:02 +0900

    mv directory

---
+++ mach/mc6800/cg/mach.c
@@ -0,0 +1,85 @@
+/* $Id$ */
+/*
+ * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
+ * See the copyright notice in the ACK home directory, in the file "Copyright".
+ */
+void con_part(int sz, word w)
+{
+	while (part_size % sz)
+		part_size++;
+	if (part_size == TEM_WSIZE)
+		part_flush();
+	if (sz == 1) {
+		w &= 0xFF;
+		if (part_size)
+			w <<= 8;
+		part_word |= w;
+	} else {
+		assert(sz == 2);
+		part_word = w;
+	}
+	part_size += sz;
+}
+
+void con_mult(word sz)
+{
+	if (sz != 4)
+		fatal("bad icon/ucon size");
+	fprintf(codefile,".data4 %ld\n", atol(str));
+}
+
+
+void con_float(void)
+{
+static int been_here;
+	if (argval != 4 && argval != 8)
+		fatal("bad fcon size");
+	fprintf(codefile,".data4\t");
+	if (argval == 8)
+		fprintf(codefile,"F_DUM,");
+	fprintf(codefile,"F_DUM\n");
+	if ( !been_here++)
+	{
+	fprintf(stderr,"Warning : dummy float-constant(s)\n");
+	}
+}
+
+void prolog(full nlocals)
+{
+	fprintf(codefile,"\tjsr Pro\n");
+	if (nlocals == 0)
+		return;
+	else
+		fprintf(codefile,
+	"\tldx #[%d].h\n\tlda #[%d].l\n\tjsr Lcs\n",
+					nlocals, nlocals);
+}
+
+void mes(word type)
+{
+	int argt ;
+	switch ( (int)type ) {
+	case ms_ext :
+		for (;;) {
+			switch ( argt=getarg(
+			    ptyp(sp_cend)|ptyp(sp_pnam)|sym_ptyp) ) {
+			case sp_cend :
+				return ;
+			default:
+				strarg(argt) ;
+				fprintf(codefile,".define %s\n",argstr) ;
+				break ;
+			}
+		}
+	default :
+		while ( getarg(any_ptyp) != sp_cend ) ;
+		break ;
+	}
+}
+
+char    *segname[] = {
+	".sect .text",        /* SEGTXT */
+	".sect .data",        /* SEGCON */
+	".sect .rom",        /* SEGROM */
+	".sect .bss"          /* SEGBSS */
+};
