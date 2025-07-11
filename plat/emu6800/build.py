75c15c1bcad4319e88d4a396fb626854d774b907
zu2 <3023910+zu2@users.noreply.github.com>
2025-07-12 01:18:08 +0900

    add file

---
+++ plat/emu6800/build.py
@@ -0,0 +1,33 @@
+from build.ab import export
+from build.ack import ackcfile, exportheaders
+from mach.proto.cg.build import build_cg
+#from plat.build import build_plat_libs
+import importlib
+
+build_as = importlib.import_module("mach.proto.as.build").build_as
+
+cflags = ["-DUSE_I80_RSTS"]
+
+build_as(name="as", arch="6800")
+build_cg(name="cg", arch="6800")
+#build_plat_libs(name="plat_libs", arch="6800", plat="emu6800")
+
+ackcfile(name="boot", srcs=["./boot.s"], plat="emu6800", cflags=cflags)
+
+export(
+    name="tools",
+    items={
+        "$(PLATDEP)/emu6800/as$(EXT)": ".+as",
+        "$(PLATDEP)/emu6800/cg$(EXT)": ".+cg",
+        "$(PLATIND)/descr/emu6800": "./descr",
+    },
+)
+
+export(
+    name="all",
+    items={
+        "$(PLATIND)/emu6800/boot.o": ".+boot",
+    }
+    | exportheaders("./include", prefix="$(PLATIND)/emu6800/include"),
+    deps=[".+tools", "util/ack+all"],
+)
