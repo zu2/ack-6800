27b2e9c116aa9b479c51ed7e8e9dc9f112a02e4c
zu2 <3023910+zu2@users.noreply.github.com>
2025-07-15 01:36:23 +0900

    add file

---
+++ mach/mc6800/libem/pro.s
@@ -0,0 +1,28 @@
+.define Pro
+.sect .text
+.sect .rom
+.sect .data
+.sect .bss
+.sect .text
+
+! This routine is called at the entry of a procedure.
+! It saves the localbase of the invoking procedure, and sets the
+! new localbase to the present value of the stackpointer.
+! It then initializes the second localbase by subtracting
+! BASE from the real one.
+
+
+Pro:
+	ldab LB+1	! get localbase (lowbyte)
+	ldaa LB		! get localbase (highbyte)
+	pshb
+	psha		! push localbase onto the stack
+	tsx		! get stackpointer
+	stx LB		! new localbase
+	ldab LB+1
+	ldaa LB
+	subb #BASE
+	sbca #0
+	stab LBl+1	! second localbase (lowbyte)
+	staa LBl	! second localbase (highbyte)
+    	rts
---
+++ mach/mc6800/libmon/head_em.s
@@ -0,0 +1,37 @@
+.define BASE
+.define hol0, ADDR
+.define LB, LBl
+.define	ARTH, RETURN
+.define start
+
+.sect .zero
+.sect .text
+.sect .rom
+.sect .data
+.sect .bss
+.sect .text
+
+BASE    = 240
+
+.sect .zero
+hol0:   .space 16       ! the hol0 block
+ADDR: .space 4          ! used for indirect addressing
+LB: .space 2            ! the localbase
+LBl: .space 2           ! the second localbase (localbase-BASE)
+ARTH: .space 16         ! used for arithmetic
+
+RETURN: .space 4        ! the return area
+
+.sect .text
+!! .base 0x0100            ! where to start in the emu6800
+! GENERAL PURPOSE ROUTINES
+start:
+	jsr	_main
+	rts
+
+.sect .data
+PROGNAME:               ! for initialising the programname pointer
+.asciz "program"
+.sect .bss
+beginbss:
+
