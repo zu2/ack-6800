733735b7692e2c901d0982e539f5a63432412572
zu2 <3023910+zu2@users.noreply.github.com>
2025-07-15 01:17:32 +0900

    add libem, libmon

--- plat/emu6800/build.py
+++ plat/emu6800/build.py
@@ -1,8 +1,23 @@
 from build.ab import export
 from build.ack import ackcfile, exportheaders
 from mach.proto.cg.build import build_cg
-#from plat.build import build_plat_libs
+from build.ab import Rule, export
 import importlib
+
+@Rule
+def build_custom_plat_libs(self, name, arch, plat):
+    export(
+        replaces=self,
+        items={
+            "$(PLATIND)/emu6800/libend.a": "mach/mc6800/libend+lib_emu6800",
+            "$(PLATIND)/emu6800/libem.a": "mach/mc6800/libem+lib_emu6800",
+            "$(PLATIND)/emu6800/libmon.a": "mach/mc6800/libmon+lib_emu6800",
+        },
+        deps=[
+        ],
+    )
+
+
 
 build_as = importlib.import_module("mach.proto.as.build").build_as
 
@@ -10,7 +25,7 @@ cflags = ["-DMC6800"]
 
 build_as(name="as", arch="mc6800")
 build_cg(name="cg", arch="mc6800")
-#build_plat_libs(name="plat_libs", arch="6800", plat="emu6800")
+build_custom_plat_libs(name="my_plat_libs", arch="mc6800", plat="emu6800")
 
 ackcfile(name="boot", srcs=["./boot.s"], plat="emu6800", cflags=cflags)
 
@@ -27,6 +42,10 @@ export(
     name="all",
     items={
         "$(PLATIND)/emu6800/boot.o": ".+boot",
+        "$(PLATIND)/emu6800/libend.a": "mach/mc6800/libend+lib_emu6800",
+        "$(PLATIND)/emu6800/libem.a": "mach/mc6800/libem+lib_emu6800",
+        "$(PLATIND)/emu6800/libmon.a": "mach/mc6800/libmon+lib_emu6800",
+
     }
     | exportheaders("./include", prefix="$(PLATIND)/emu6800/include"),
     deps=[".+tools", "util/ack+all"],
