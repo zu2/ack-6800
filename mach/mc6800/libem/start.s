46787f3664a1f1c110f89a0dd07eb15d0e706ff5
zu2 <3023910+zu2@users.noreply.github.com>
2025-07-17 02:35:57 +0900

    add start up

---
+++ mach/mc6800/libem/start.s
@@ -0,0 +1,33 @@
+.define BASE
+.define hol0, ADDR
+.define LB, LBl
+.define	ARTH, RETURN
+.define START
+
+.sect .zero
+.sect .text
+.sect .rom
+.sect .data
+.sect .bss
+.sect .end
+
+BASE    = 240
+
+.sect .zero
+hol0:   .space 16       ! the hol0 block
+ADDR: .space 4          ! used for indirect addressing
+LB: .space 2            ! the localbase
+LBl: .space 2           ! the second localbase (localbase-BASE)
+ARTH: .space 16         ! used for arithmetic
+RETURN: .space 4        ! the return area
+
+.sect .text
+!.base 0x0100            ! where to start in the emu6800
+! GENERAL PURPOSE ROUTINES
+START:  jmp     _main
+
+.sect .data
+PROGNAME:               ! for initialising the programname pointer
+.asciz "program"
+.sect .bss
+beginbss:
