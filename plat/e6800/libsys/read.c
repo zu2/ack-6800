b57a5152f5c055c5c1c32309b067c490f3b0b9ed
zu2 <3023910+zu2@users.noreply.github.com>
2025-07-28 12:39:01 +0900

    add console read.

--- plat/e6800/libsys/read.c
+++ plat/e6800/libsys/read.c
@@ -12,6 +12,16 @@
 
 ssize_t read(int fd, void* buffer, size_t count)
 {
+	if (fd == 0)
+	{
+		/* Read from the console. */
+
+		if (count == 0)
+			return 0;
+		*(uint8_t*)buffer = emu6800_conin();
+		return 1;
+	}
+
 eio:
 	errno = EIO;
 	return -1;
